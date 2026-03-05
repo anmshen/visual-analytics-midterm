ce6a851811081884de6f4e532f2a097fda7e69f7
anmshen <anshen017@gmail.com>
2026-03-05 17:31:04 -0500

    first work session

--- app.py
+++ app.py
@@ -1,12 +1,74 @@
 from flask import Flask, render_template, request
-import pandas as pd
+import duckdb
 
 app = Flask(__name__)
+continuous_columns = ['humidity', 'temp', 'wind']
+discrete_columns = ['day']
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+sorted_months = sorted(months)
 
-data = pd.read_csv("placementdata.csv")
-app.data = data
+@app.route('/')
+def index():
+    scatter_ranges_query = f'SELECT MIN(X), MAX(X), MIN(Y), MAX(Y) FROM forestfires.csv' # Retrieves the minimum and maximum X and Y coordinates
+    scatter_ranges_results = duckdb.sql(scatter_ranges_query).df() 
+    scatter_ranges = scatter_ranges_results.iloc[0].to_list() # TODO: Define a list [minimum of X, maximum of X, minimum of Y, maximum of Y]
+      
+    max_count_query = '''
+        SELECT MAX(month_count) 
+        FROM (
+            SELECT COUNT(*) AS month_count 
+            FROM 'forestfires.csv' 
+            GROUP BY month
+        ) AS maxFFCount
+    ''' # TODO: Write a query that retrieves the maximum number of forest fires that occurred in a single month
+    max_count_results = duckdb.sql(max_count_query).df()
+    max_count = int(max_count_results.iloc[0, 0]) # TODO: Extract the maximum count from the query results
 
-# more stuff here 
+    # TODO: write a query that retrieves the the minimum and maximum value for each slider
+    humid_min_max_query = 'SELECT MIN(humidity), MAX(humidity)  FROM forestfires.csv' 
+    filter_humid_ranges = duckdb.sql(humid_min_max_query).df()
+    temp_min_max_query = 'SELECT MIN(temp), MAX(temp)  FROM forestfires.csv' 
+    filter_temp_ranges = duckdb.sql(temp_min_max_query).df()
+    wind_min_max_query = 'SELECT MIN(wind), MAX(wind) FROM forestfires.csv' 
+    filter_wind_ranges = duckdb.sql(wind_min_max_query).df()
+    filter_ranges = {'humidity': filter_humid_ranges.iloc[0].to_list(), 
+                     'temp': filter_temp_ranges.iloc[0].to_list(), 
+                     'wind': filter_wind_ranges.iloc[0].to_list()} # TODO: Create a dictionary where each key is a filter and values are the minimum and maximum values
+    
+    return render_template(
+        'index.html', months=months, days=days,
+        filter_ranges=filter_ranges, scatter_ranges=scatter_ranges, max_count=max_count
+    )
+
+@app.route('/update', methods=["POST"])
+def update():
+    request_data = request.get_json()
+    
+    continuous_predicate = ' AND '.join([f'({column} >= {request_data[column][0]} AND {column} <= {request_data[column][1]})' for column in continuous_columns]) # TODO: update where clause from sliders
+    days_list = "', '".join(request_data['day'])
+    discrete_predicate = f"day IN ('{days_list}')"
+    # discrete_predicate = ' AND '.join([f'{column} IN ({days})' for column in discrete_columns]) # TODO: update where clause from checkboxes
+    predicate = ' AND '.join([continuous_predicate, discrete_predicate]) # Combine where clause from sliders and checkboxes
+
+    scatter_query = f'SELECT X, Y FROM forestfires.csv WHERE {predicate}'
+    scatter_results = duckdb.sql(scatter_query).df()
+    scatter_data = scatter_results.to_dict(orient='records') # TODO: Extract the data that will populate the scatter plot
+
+    bar_query = f'''
+                SELECT month, COUNT(*) as count
+                FROM "forestfires.csv"
+                WHERE {predicate}
+                GROUP BY month
+                ''' # TODO: Write a query that retrieves the number of forest fires per month after filtering
+    bar_results = duckdb.sql(bar_query).df()
+    # bar_results['month'] = bar_results.index.map({i: sorted_months[i] for i in range(len(sorted_months))})
+    counts_dict = dict(zip(bar_results['month'], bar_results['count']))
+    bar_data = [int(counts_dict.get(m.lower(), 0)) for m in months] # TODO: Extract the data that will populate the bar chart from the results
+    max_count = max(bar_data) if bar_data else 0 # TODO: Extract the maximum number of forest fires in a single month from the results
+    
+    return {'scatter_data': scatter_data, 'bar_data': bar_data, 'max_count': max_count}
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    app.run(debug=True)
+    
---
+++ static/js/script.js
@@ -0,0 +1,147 @@
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function draw_svg(container_id, margin, width, height){
+    svg = d3.select("#"+container_id)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("background-color", "#dbdad7")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    return svg
+}
+
+function draw_xaxis(plot_name, svg, height, scale){
+    svg.append("g")
+        .attr('class', plot_name + "-xaxis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(scale).tickSize(0))
+}
+
+function draw_yaxis(plot_name, svg, scale){
+    svg.append("g")
+        .attr('class', plot_name + "-yaxis")
+        .call(d3.axisLeft(scale));
+}
+
+function draw_axis(plot_name, axis, svg, height, domain, range, discrete){
+    if (discrete){
+        var scale = d3.scaleBand()
+            .domain(domain)
+            .range(range)
+            .padding([0.2])
+    } else {
+        var scale = d3.scaleLinear()
+            .domain(domain)
+            .range(range);
+    }
+    if (axis=='x'){
+        draw_xaxis(plot_name, svg, height, scale)
+    } else if (axis=='y'){
+        draw_yaxis(plot_name, svg, scale)
+    }
+    return scale
+}
+
+function draw_axes(plot_name, svg, width, height, domainx, domainy, x_discrete){
+    var x_scale = draw_axis(plot_name, 'x', svg, height, domainx, [0, width], x_discrete)
+    var y_scale = draw_axis(plot_name, 'y', svg, height, domainy, [height, 0], false)
+    return {'x': x_scale, 'y': y_scale}
+}
+
+function draw_slider(column, min, max, scatter_svg, bar_svg, scatter_scale, bar_scale){
+    slider = document.getElementById(column+'-slider')
+    noUiSlider.create(slider, {
+      start: [min, max],
+      connect: false,
+          tooltips: true,
+      step: 1,
+      range: {'min': min, 'max': max}
+    });
+    slider.noUiSlider.on('change', function(){
+        update(scatter_svg, bar_svg, scatter_scale, bar_scale)
+    });
+}
+
+// TODO: Write a function that draws the scatter plot
+function draw_scatter(data, svg, scale) {
+    svg.selectAll(".dot")
+        .data(data)
+        .join(
+            enter => enter.append("circle")
+                .attr("class", "dot")
+                .attr("r", 4)
+                .attr("cx", d => scale.x(d.X))
+                .attr("cy", d => scale.y(d.Y))
+                .style("fill", "#bc2929")
+                .style("stroke", "black")
+                .style("stroke-width", "1px"),
+            update => update
+                .attr("cx", d => scale.x(d.X))
+                .attr("cy", d => scale.y(d.Y)),
+            exit => exit.remove()
+        );
+}
+
+// TODO: write a function that updates the bar
+function draw_bar(data, svg, scale) {
+    const barHeight = scale.y.range()[0];
+    svg.selectAll(".bar")
+        .data(data)
+        .join(
+            enter => enter.append("rect")
+                .attr("class", "bar")
+                .attr("x", (d, i) => scale.x(months[i])) 
+                .attr("width", scale.x.bandwidth())
+                .attr("y", d => scale.y(d))
+                .attr("height", d => barHeight - scale.y(d))
+                .style("fill", "#3f7cae"),
+            update => update
+                .attr("y", d => scale.y(d))
+                .attr("height", d => barHeight - scale.y(d)),
+            exit => exit.remove()
+        );
+}
+
+// TODO: Write a function that extracts the selected days and minimum/maximum values for each slider
+function get_params(){
+    var day = []
+    d3.selectAll('input.checkboxDays:checked').each(function() {
+        day.push(this.value);
+    });
+    var humidity = document.getElementById('humidity-slider').noUiSlider.get().map(Number);
+    var temp = document.getElementById('temp-slider').noUiSlider.get().map(Number);
+    var wind = document.getElementById('wind-slider').noUiSlider.get().map(Number);
+    return {'day': day, 'humidity': humidity, 'temp': temp, 'wind': wind}
+}
+
+// TODO: Write a function that removes the old data points and redraws the scatterplot
+function update_scatter(data, svg, scale){
+    svg.selectAll(".dot").remove();
+    draw_scatter(data, svg, scale)
+}
+
+// TODO: Write a function that updates the y-axis, removes the old bars, and redraws the bars
+function update_bar(data, max_count, svg, scale){
+    svg.selectAll(".bar").remove();
+    svg.selectAll(".bar-yaxis").remove();
+    scale.y = draw_axis('bar', 'y', svg, height, [0, max_count], [height, 0], false);
+    draw_bar(data, svg, scale)
+}
+
+function update(scatter_svg, bar_svg, scatter_scale, bar_scale){
+    params = get_params()
+    fetch('/update', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(params),
+        cache: 'no-cache',
+        headers: new Headers({
+            'content-type': 'application/json'
+        })
+    }).then(async function(response){
+        var results = JSON.parse(JSON.stringify((await response.json())))
+        update_scatter(results['scatter_data'], scatter_svg, scatter_scale)
+        update_bar(results['bar_data'], results['max_count'], bar_svg, bar_scale)
+    })
+}
