89746a40858264e31eea05dd5dc593dfbf72db90
anmshen <anshen017@gmail.com>
2026-03-08 16:57:02 -0400

    sun work session

--- app.py
+++ app.py
@@ -4,29 +4,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 continuous_columns = ['Internships', 'Projects', 'AptitudeTestScore', 'SoftSkillsRating', 'SSC_Marks', 'HSC_Marks']
 discrete_columns = ['ExtracurricularActivities, PlacementTraining']
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
-sorted_months = sorted(months)
 
 @app.route('/')
 def index():
+    # get x and y axis min/max values 
     scatter_ranges_query = f'SELECT MIN(AptitudeTestScore), MAX(AptitudeTestScore), MIN(CGPA), MAX(CGPA) FROM placementdata.csv' # Retrieves the minimum and maximum X and Y coordinates
     scatter_ranges_results = duckdb.sql(scatter_ranges_query).df() 
     scatter_ranges = scatter_ranges_results.iloc[0].to_list()
-      
-    #TODO remove max count
-    max_count_query = '''
-        SELECT MAX(month_count) 
-        FROM (
-            SELECT COUNT(*) AS month_count 
-            FROM 'forestfires.csv' 
-            GROUP BY month
-        ) AS maxFFCount
-    ''' # TODO: Write a query that retrieves the maximum number of forest fires that occurred in a single month
-    max_count_results = duckdb.sql(max_count_query).df()
-    max_count = int(max_count_results.iloc[0, 0]) # TODO: Extract the maximum count from the query results
 
-    # TODO: write a query that retrieves the the minimum and maximum value for each slider
+    # get minimum and maximum value for each slider
     internship_min_max_query = 'SELECT MIN(Internships), MAX(Internships) FROM placementdata.csv'
     filter_internship_ranges = duckdb.sql(internship_min_max_query).df()
 
@@ -45,6 +31,7 @@ def index():
     hsc_min_max_query = 'SELECT MIN(HSC_Marks), MAX(HSC_Marks) FROM placementdata.csv' 
     filter_hsc_ranges = duckdb.sql(hsc_min_max_query).df()
     
+    # dict of all slider ranges
     filter_ranges = {'internships': filter_internship_ranges.iloc[0].to_list(), 
                      'projects': filter_projects_ranges.iloc[0].to_list(), 
                      'aptitude': filter_aptitude_ranges.iloc[0].to_list(),
@@ -53,28 +40,68 @@ def index():
                      'hsc-marks': filter_hsc_ranges.iloc[0].to_list()} 
     
     return render_template(
-        'index.html', months=months, days=days,
-        filter_ranges=filter_ranges, scatter_ranges=scatter_ranges, max_count=0
+        'index.html',
+        filter_ranges=filter_ranges, 
+        scatter_ranges=scatter_ranges,
+        x_vals=['FIX THIS LATER', "x val 2"],
+        y_vals=['FIX THIS LATER', "FIX 2"],
+        facets = ['FIX THIS LATER', 'facet 2']
     )
+
+
+# TODO: Complete the update_aggregate
+@app.route("/update_facet", methods=["POST"])
+def update_facet():
+    data = request.get_json()
+    print(data)
+
+    # app.grouper = data.get("grouper", app.grouper)
+    # app.value = data.get("value", app.value)
+    # app.agg = data.get("agg", app.agg)
+
+    # # Reset to all 3b
+    # app.filters = {}
+
+    # aggregated_data = get_aggregated_data()
+
+    # result = [{"x": str(key), "y": float(value)}
+    #           for key, value in aggregated_data.items()]
+
+    # return {'data': result, 'x_column': app.grouper}
+
+    # idea: if x and y change, then we need to just update what we pass as the data for x and y/column to javascript
+    return {'data': [], 'x_column': ""}
 
 @app.route('/update', methods=["POST"])
 def update():
     request_data = request.get_json()
     print(request_data)
     
-    continuous_predicate = ' AND '.join([f'({column} >= {request_data[column][0]} AND {column} <= {request_data[column][1]})' for column in continuous_columns]) # TODO: update where clause from sliders
-    # days_list = "', '".join(request_data['day'])
-    # discrete_predicate = f"day IN ('{days_list}')"
-    # discrete_predicate = ' AND '.join([f'{column} IN ({days})' for column in discrete_columns]) # TODO: update where clause from checkboxes
-    # predicate = ' AND '.join([continuous_predicate, discrete_predicate]) # Combine where clause from sliders and checkboxes
-    # predicate = ' AND '.join([continuous_predicate]) # Combine where clause from sliders and checkboxes
-    placed_predicate = ' AND '.join([continuous_predicate, "PlacementStatus == True"])
-    notplaced_predicate = ' AND '.join([continuous_predicate, "PlacementStatus == False"])
+    # parameterized query for sliders
+    continuous_predicate = ' AND '.join([f'({column} >= {request_data[column][0]} AND {column} <= {request_data[column][1]})' for column in continuous_columns])
+    
+    # paramaterized query for buttons
+    extraval = "Yes"
+    if request_data["Extracurricular_Activities"] == False:
+        extraval = "No"
 
+    placeval = "Yes"
+    if request_data["Placement_Training"] == False:
+        placeval = "No"
+        
+    discrete_predicate = f"(ExtracurricularActivities == '{extraval}') AND (PlacementTraining == '{placeval}')"
+
+    combined_predicate = ' AND '.join([continuous_predicate, discrete_predicate])
+    
+    placed_predicate = ' AND '.join([combined_predicate, "PlacementStatus == 'Placed'"])
+    notplaced_predicate = ' AND '.join([combined_predicate, "PlacementStatus == 'NotPlaced'"])
+
+    # placed data
     scatter_placed_query = f'SELECT AptitudeTestScore, CGPA FROM placementdata.csv WHERE {placed_predicate}'
     scatter_placed_results = duckdb.sql(scatter_placed_query).df()
     scatter_placed_data = scatter_placed_results.to_dict(orient='records') 
-
+    
+    # not placed data
     scatter_notplaced_query = f'SELECT AptitudeTestScore, CGPA FROM placementdata.csv WHERE {notplaced_predicate}'
     scatter_notplaced_results = duckdb.sql(scatter_notplaced_query).df()
     scatter_notplaced_data = scatter_notplaced_results.to_dict(orient='records') 
--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,48 @@
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+let currentX = "AptitudeTestScore";
+let currentY = "CGPA";
 
 function draw_svg(container_id, margin, width, height){
     svg = d3.select("#"+container_id)
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .style("background-color", "#dbdad7")
+    .style("background-color", "#dce0e4")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     return svg
 }
 
-function draw_xaxis(plot_name, svg, height, scale){
+function draw_xaxis(plot_name, svg, height, scale, label="x"){
     svg.append("g")
         .attr('class', plot_name + "-xaxis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(scale).tickSize(0))
+        .call(d3.axisBottom(scale).tickSize(5).tickPadding(10));
+
+    svg.append("text")
+        .attr("class", "x-label")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + 40)
+        .text(label)
+        .style("font-size", "14px")
+        .style("font-weight", "bold");
 }
 
-function draw_yaxis(plot_name, svg, scale){
+function draw_yaxis(plot_name, svg, scale, label="y"){
     svg.append("g")
         .attr('class', plot_name + "-yaxis")
-        .call(d3.axisLeft(scale));
+        .call(d3.axisLeft(scale).tickSize(5).tickPadding(10));
+
+    svg.append("text")
+        .attr("class", "y-label")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -45)
+        .attr("x", -height / 2)
+        .text(label)
+        .style("font-size", "14px")
+        .style("font-weight", "bold");
 }
 
 function draw_axis(plot_name, axis, svg, height, domain, range, discrete){
@@ -36,16 +57,21 @@ function draw_axis(plot_name, axis, svg, height, domain, range, discrete){
             .range(range);
     }
     if (axis=='x'){
-        draw_xaxis(plot_name, svg, height, scale)
+        draw_xaxis(plot_name, svg, height, scale, label=currentX)
     } else if (axis=='y'){
-        draw_yaxis(plot_name, svg, scale)
+        draw_yaxis(plot_name, svg, scale, label=currentY)
     }
     return scale
 }
 
 function draw_axes(plot_name, svg, width, height, domainx, domainy, x_discrete){
-    var x_scale = draw_axis(plot_name, 'x', svg, height, domainx, [0, width], x_discrete)
-    var y_scale = draw_axis(plot_name, 'y', svg, height, domainy, [height, 0], false)
+    let x_buffer = (domainx[1] - domainx[0]) * 0.1;
+    let padded_x = [domainx[0] - x_buffer, domainx[1] + x_buffer];
+    let y_buffer = (domainy[1] - domainy[0]) * 0.1;
+    let padded_y = [domainy[0] - y_buffer, domainy[1] + y_buffer];
+
+    var x_scale = draw_axis(plot_name, 'x', svg, height, padded_x, [0, width], false)
+    var y_scale = draw_axis(plot_name, 'y', svg, height, padded_y, [height, 0], false)
     return {'x': x_scale, 'y': y_scale}
 }
 
@@ -64,6 +90,8 @@ function draw_slider(column, min, max, scatter_svg, bar_svg, scatter_scale, bar_
 }
 
 // TODO: Write a function that draws the scatter plot
+
+
 function draw_scatter(data, svg, scale) {
     svg.selectAll(".dot")
         .data(data)
@@ -71,62 +99,58 @@ function draw_scatter(data, svg, scale) {
             enter => enter.append("circle")
                 .attr("class", "dot")
                 .attr("r", 4)
-                .attr("cx", d => scale.x(d.X))
-                .attr("cy", d => scale.y(d.Y))
-                .style("fill", "#bc2929")
-                .style("stroke", "black")
-                .style("stroke-width", "1px"),
+                .attr("cx", d => scale.x(d.AptitudeTestScore))
+                .attr("cy", d => scale.y(d.CGPA))
+                .attr('opacity', 0.7)
+                .style("fill", "#3c7ab0")
+                .style("stroke", "none"),
             update => update
-                .attr("cx", d => scale.x(d.X))
-                .attr("cy", d => scale.y(d.Y)),
-            exit => exit.remove()
-        );
-}
-
-// TODO: write a function that updates the bar
-function draw_bar(data, svg, scale) {
-    const barHeight = scale.y.range()[0];
-    svg.selectAll(".bar")
-        .data(data)
-        .join(
-            enter => enter.append("rect")
-                .attr("class", "bar")
-                .attr("x", (d, i) => scale.x(months[i])) 
-                .attr("width", scale.x.bandwidth())
-                .attr("y", d => scale.y(d))
-                .attr("height", d => barHeight - scale.y(d))
-                .style("fill", "#3f7cae"),
-            update => update
-                .attr("y", d => scale.y(d))
-                .attr("height", d => barHeight - scale.y(d)),
+                .attr("cx", d => scale.x(d.AptitudeTestScore))
+                .attr("cy", d => scale.y(d.CGPA)),
             exit => exit.remove()
         );
 }
 
 // TODO: Write a function that extracts the selected days and minimum/maximum values for each slider
 function get_params(){
-    var day = []
-    d3.selectAll('input.checkboxDays:checked').each(function() {
-        day.push(this.value);
-    });
-    var humidity = document.getElementById('humidity-slider').noUiSlider.get().map(Number);
-    var temp = document.getElementById('temp-slider').noUiSlider.get().map(Number);
-    var wind = document.getElementById('wind-slider').noUiSlider.get().map(Number);
-    return {'day': day, 'humidity': humidity, 'temp': temp, 'wind': wind}
+    return {
+        'Internships': document.getElementById('internships-slider').noUiSlider.get().map(Number),
+        'Projects': document.getElementById('projects-slider').noUiSlider.get().map(Number),
+        'AptitudeTestScore': document.getElementById('aptitude-slider').noUiSlider.get().map(Number),
+        'SoftSkillsRating': document.getElementById('soft-slider').noUiSlider.get().map(Number),
+        'SSC_Marks': document.getElementById('ssc-marks-slider').noUiSlider.get().map(Number),
+        'HSC_Marks': document.getElementById('hsc-marks-slider').noUiSlider.get().map(Number),
+        'Extracurricular_Activities': document.querySelector('input[name="extrabutton"]:checked').value,
+        'Placement_Training': document.querySelector('input[name="placementbutton"]:checked').value
+    }
+}
+
+function update_facet(value, type){    
+    const x_val = document.getElementById("xdropdown").value;
+    const y_val = document.getElementById("ydropdown").value;
+    const facet = document.getElementById("facet").value;
+    // document.getElementById("Country/Region-filter").value = "All";
+    // document.getElementById("Region-filter").value = "All";
+    // document.getElementById("State/Province-filter").value = "All";
+
+    fetch('/update_facet', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({"x_val": x_val, "y_val": y_val, "facet": facet}),
+        cache: 'no-cache',
+        headers: new Headers({
+            'content-type': 'application/json'
+        })
+    }).then(async function(response){
+        var results = JSON.parse(JSON.stringify((await response.json())))
+        // TODO: DRAW THE SCATTER PLOT
+    })
 }
 
 // TODO: Write a function that removes the old data points and redraws the scatterplot
 function update_scatter(data, svg, scale){
     svg.selectAll(".dot").remove();
     draw_scatter(data, svg, scale)
-}
-
-// TODO: Write a function that updates the y-axis, removes the old bars, and redraws the bars
-function update_bar(data, max_count, svg, scale){
-    svg.selectAll(".bar").remove();
-    svg.selectAll(".bar-yaxis").remove();
-    scale.y = draw_axis('bar', 'y', svg, height, [0, max_count], [height, 0], false);
-    draw_bar(data, svg, scale)
 }
 
 function update(scatter_svg, bar_svg, scatter_scale, bar_scale){
@@ -141,7 +165,7 @@ function update(scatter_svg, bar_svg, scatter_scale, bar_scale){
         })
     }).then(async function(response){
         var results = JSON.parse(JSON.stringify((await response.json())))
-        update_scatter(results['scatter_data'], scatter_svg, scatter_scale)
-        update_bar(results['bar_data'], results['max_count'], bar_svg, bar_scale)
+        update_scatter(results['scatter_placed_data'], scatter_placed_svg, scatter_placed_scale)
+        update_scatter(results['scatter_notplaced_data'], scatter_notplaced_svg, scatter_notplaced_scale)
     })
 }
